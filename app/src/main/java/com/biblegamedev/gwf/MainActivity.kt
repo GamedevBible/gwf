645362ab38b76b3f51e8a81234ccceaceec6ccf8
biblegamedev <biblegamedev@gmail.com>
2020-01-02 15:35:16 +0700

    working on main screen

--- app/src/main/java/com/biblegamedev/gwf/MainActivity.kt
+++ app/src/main/java/com/biblegamedev/gwf/MainActivity.kt
@@ -2,11 +2,24 @@ package com.biblegamedev.gwf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mDayNightMode = AppCompatDelegate.getDefaultNightMode();
+
+        aliasButton.setOnClickListener {
+            mDayNightMode = if (mDayNightMode == AppCompatDelegate.MODE_NIGHT_NO)
+                AppCompatDelegate.MODE_NIGHT_YES
+            else
+                AppCompatDelegate.MODE_NIGHT_NO
+            AppCompatDelegate.setDefaultNightMode(mDayNightMode)
+            delegate.applyDayNight()
+        }
     }
 }
